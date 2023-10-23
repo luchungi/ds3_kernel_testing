1981e9c0106a61fcee05c615cdd814e9c77df104
Lu Chung I <lu.chung.i@gmail.com>
2023-10-23 17:27:38 +0800

    OU and Heston paths with quantlib

--- sigkernel/tests.py
+++ sigkernel/tests.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import brentq
+import QuantLib as ql
 
 from .kernels import SignatureKernel
 from .static.kernels import LinearKernel
@@ -142,13 +143,13 @@ def get_permuted_cross_kernel_sum(K_XX, K_YY, K_XY, inds_X_1, inds_Y_1, inds_X_2
                 np.sum(K_XY[np.ix_(inds_X_1, inds_Y_2)]) +
                 np.sum(K_XY[np.ix_(inds_X_2, inds_Y_1)]))
 
-def sig_kernel_test(X, Y, n_levels, static_kernel, num_permutations=1000, a=1, C=4, stats_plot=True, quantile=0.95, ratio_plot=True, n_steps=10):
+def sig_kernel_test(X, Y, n_levels, static_kernel, num_permutations=1000, a=1, C=4, stats_plot=True, percentile=0.9, ratio_plot=True, n_steps=10):
     '''
     X: np.array of shape (n_samples, n_features)
     Y: np.array of shape (n_samples, n_features)
     n_levels: int, number of levels of the signature kernel where level 0 is not included e.g. 3 means signature (t0, t1, t2, t3)
     static_kernel: static kernel to be lifted to sequence kernel e.g. LinearKernel or RBFKernel
-    quantile: float, quantile to be used for the test
+    percentile: float, percentile to be used for the test
     num_permutations: int, number of permutations to be used to generate the null distribution
     a: float, parameter for psi function used in the tensor normalisation to get the characteristic signature kernel
     C: float, as above
@@ -174,7 +175,7 @@ def sig_kernel_test(X, Y, n_levels, static_kernel, num_permutations=1000, a=1, C
         null_mmds[i] = perm_K_XX_sum / (n*(n-1))  + perm_K_YY_sum / (m*(m-1))  - 2*perm_K_XY_sum/(n*m)
 
     if stats_plot:
-        plot_permutation_samples(null_mmds, statistic=mmd, percentile=quantile)
+        plot_permutation_samples(null_mmds, statistic=mmd, percentile=percentile)
 
     if ratio_plot:
         plt.figure()
@@ -247,15 +248,14 @@ def mmd_permutation_ratio_plot(X, Y, n_levels, static_kernel, n_steps=10, a=1, C
     legend = ['MMD at different split ratios', 'Actual test statistic']
     plt.legend(legend)
 
-def plot_permutation_samples(null_samples, statistic=None, percentile=0.95, two_tailed=False):
+def plot_permutation_samples(null_samples, statistic=None, percentile=0.9, two_tailed=False):
     plt.hist(null_samples, bins=100)
 
     if two_tailed:
-        plt.axvline(x=np.percentile(null_samples, 0.5 + 0.5*percentile), c='b')
-        plt.axvline(x=np.percentile(null_samples, 0.5*(1 - percentile)), c='b')
+        plt.axvline(x=np.percentile(null_samples, 50 * (1 + percentile)), c='b')
     else:
-        plt.axvline(x=np.percentile(null_samples, 100-percentile), c='b')
-    legend = [f'{int(100*percentile)}% quantiles']
+        plt.axvline(x=np.percentile(null_samples, 100*percentile), c='b')
+    legend = [f'{int(100*percentile)} percentile']
 
     if statistic is not None:
         percentile = (null_samples < statistic).sum() / len(null_samples)
@@ -275,3 +275,41 @@ def gen_GBM_path(mu, sigma, dt, n_paths, seq_len, seed=None):
     path = np.concatenate([np.ones((n_paths, 1)), path], axis=1)
     path = path[..., np.newaxis]
     return path # shape (n_paths, seq_len, 1)
+
+def gen_quantlib_paths(process, dt, n_paths, seq_len, seed, return_all_paths):
+
+    times = ql.TimeGrid((seq_len-1)*dt, seq_len-1) # creates list of times starting from 0 to (seq_len-1)*dt with step size dt
+    dimension = process.factors() # 2 factors for Heston model i.e. spot and vol
+
+    randomGenerator = ql.UniformRandomGenerator() if seed is None else ql.UniformRandomGenerator(seed=seed) # seed of 0 seems to not set a seed
+    rng = ql.UniformRandomSequenceGenerator(dimension * (seq_len-1), randomGenerator)
+    sequenceGenerator = ql.GaussianRandomSequenceGenerator(rng)
+    pathGenerator = ql.GaussianMultiPathGenerator(process, list(times), sequenceGenerator, False)
+
+    paths = [[] for i in range(dimension)]
+    for _ in range(n_paths):
+        samplePath = pathGenerator.next()
+        values = samplePath.value()
+
+        for j in range(dimension):
+            paths[j].append([x for x in values[j]])
+
+    if return_all_paths:
+        return np.array(paths).transpose(1,0,2)
+    else:
+        return np.array(paths[0])[..., np.newaxis]
+
+# simulate Heston paths
+def gen_Heston_path(mu, v0, kappa, theta, rho, sigma, dt, n_paths, seq_len, seed=None, return_vols=False):
+    today = ql.Date().todaysDate()
+    riskFreeTS = ql.YieldTermStructureHandle(ql.FlatForward(today, mu, ql.Actual365Fixed()))
+    dividendTS = ql.YieldTermStructureHandle(ql.FlatForward(today, 0.00, ql.Actual365Fixed()))
+    initialValue = ql.QuoteHandle(ql.SimpleQuote(1.0))
+
+    hestonProcess = ql.HestonProcess(riskFreeTS, dividendTS, initialValue, v0, kappa, theta, sigma, rho)
+
+    return gen_quantlib_paths(hestonProcess, dt, n_paths, seq_len, seed=seed, return_all_paths=return_vols)
+
+def gen_OU_path(kappa, theta, sigma, dt, n_paths, seq_len, seed=None):
+    process = ql.ExtendedOrnsteinUhlenbeckProcess(kappa, sigma, 1.0, lambda x: theta)
+    return gen_quantlib_paths(process, dt, n_paths, seq_len, seed=seed, return_all_paths=False)
